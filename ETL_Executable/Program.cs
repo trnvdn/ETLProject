24f4da8804b9670669f550f9602fb6b61f366755
Daniil <91062667+trnvdn@users.noreply.github.com>
2024-05-17 16:08:00 +0300

    Add project files.

---
+++ ETL_Executable/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ETL_Executable
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
